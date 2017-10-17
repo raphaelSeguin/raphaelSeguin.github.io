a1cf0e97ca5bddffbe6a3174c09c7135b9a8ef8a
raphaelSeguin <raphaelseguin@gmail.com>
2017-10-18 00:26:17 +0200

    ok
    fixed

--- ChaosGame/sketch.js
+++ ChaosGame/sketch.js
@@ -1,18 +1,20 @@
 var Polygon;
 var poly;
 var pointsPerFrame = 500;
-var edges = 4;
+var edges = 9;
 var rules = [
-  [0]
+  [1, 8], [5, 4], [2, 7]
 ];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  pixelDensity(2.0);
+  //frameRate(1);
   Polygon = function(edges, radius) {
     this.edges = edges;
     this.vertices = [];
     this.currentPoint = [0, 0];
-    this.pastPoints = [];
+    this.pastVertexNumbers = [];
     this.rules = [];
     this.init = function() {
       // fill vertices
@@ -22,13 +24,15 @@ function setup() {
         var y = sin(angle) * radius;
         this.vertices[i] = [x, y];
       }
+      for (var i = 0; i < rules.length ; i++) {
+        this.pastVertexNumbers.push(-1);
+      }
       // put the curretn point inside the polygon at random place...
       //this.currentPoint = ???
-      this.pastPoints.shift(this.currentPoint);
       // pick some rules
-      var maxMemorySize = 4;
-      var maxRulesPerGeneration = edges - 2; // sinon on reste bloqué ???
-      var memorySize = Math.floor(Math.random() * (maxMemorySize + 1));
+      //var maxMemorySize = 4;
+      //var maxRulesPerGeneration = edges - 2; // sinon on reste bloqué ???
+      //var memorySize = Math.floor(Math.random() * (maxMemorySize + 1));
     }
     this.display = function() {
       push();
@@ -48,12 +52,18 @@ function setup() {
       translate(width / 2, height / 2);
       for (var i = 0; i < n; i++) {
         //pick a vertex according to the rules
-        var pickedVertex;
+        var pickedVertex, vertexNumber;
         do {
-          pickedVertex = this.vertices[Math.floor(Math.random() * this.edges)];
-        } while ( this.checkRules(pickedVertex) );
-        //
-        this.pastPoints.shift(pickedVertex);
+          vertexNumber = Math.floor(Math.random() * this.edges);
+        } while ( this.checkRules(vertexNumber) );
+        //console.log("vertexNumber : " + vertexNumber);
+
+        pickedVertex = this.vertices[vertexNumber];
+        this.pastVertexNumbers.unshift(vertexNumber);
+        this.pastVertexNumbers.pop(this.rules.length);
+        //console.log("pastVertexNumbers : " + this.pastVertexNumbers[0]);
+        //console.log("pastVertexNumbers : " + this.pastVertexNumbers[1]);
+        //console.log("rules.length" + this.rules.length);
         //move the current point half the way to the picked vertex
         this.currentPoint[0] += (pickedVertex[0] - this.currentPoint[0]) / 2;
         this.currentPoint[1] += (pickedVertex[1] - this.currentPoint[1]) / 2;
@@ -65,10 +75,10 @@ function setup() {
     this.setRules = function(rules) {
       this.rules = rules;
     }
-    this.checkRules = function(p) {
-      for (var i = 0; i < this.pastPoints.length; i++) {
+    this.checkRules = function( n ) {
+      for (var i = 0; i < this.pastVertexNumbers.length; i++) {
         for (var j = 0; j < this.rules[i].length; j++) {
-          if (this.rules[i][j] > -1 && p == (this.pastPoints[i] + this.rules[i][j]) % this.edges) {
+          if ( n === (this.pastVertexNumbers[i] + this.rules[i][j]) % this.edges) {
             return true;
           }
         }
@@ -76,18 +86,23 @@ function setup() {
       return false;
     }
   }
-  poly = new Polygon(edges, 300);
-  poly.init();
+  poly = new Polygon(edges, height/2 - 20);
   poly.setRules(rules);
+  poly.init();
 }
 
 function draw() {
   poly.display();
   poly.addPoints(pointsPerFrame);
+  caption();
+}
+
+function caption() {
   fill(255);
   noStroke();
   rect(0, 0, 200, 50);
   fill(0);
   text("frame rate : " + frameRate().toFixed(2), 10, 20);
   text("points : " + frameCount * pointsPerFrame, 10, 30);
+  text("edges : " + edges, 10, 40);
 }
--- Logistic/sketch.js
+++ Logistic/sketch.js
@@ -5,68 +5,82 @@ var mode = 0;
 
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+    createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
-  switch (mode) {
-    case 0:
-      background(255, 50);
-      stroke(0);
-      noFill();
-      var initVal = ( (frameCount * 0.0002) + map(mouseX, 0, width, 0, 1) ) % 1;
-      beginShape();
-      for (var x = 0; x < width; x++) {
-        var growth = map(x, 0, width, 2.8, 4.);
-        var points = logistic(initVal, growth, 10);
-        curveVertex(x, (1 - points[9]) * height);
-      }
-      endShape();
-      caption(initVal);
-      break;
-    case 1:
-      stroke(0, 20);
-      noFill();
-      var points;
-      var initVal = random(0, 1.);
-      for (var x = 0; x < width; x++) {
-        var growth = map(x, 0, width, 2.8, 4.);
-        points = logistic(initVal, growth, 20);
-        for (var index = 10; index < 20; index += 1.) {
-          point(x, (1 - points[index]) * height);
-        }
-      }
-      caption(initVal);
-      break;
-  }
+    switch (mode) {
+        case 0:
+            background(255, 50);
+            stroke(0);
+            noFill();
+            var initVal = ((frameCount * 0.0002) + map(mouseX, 0, width, 0, 1)) % 1;
+            beginShape();
+            for (var x = 0; x < width; x++) {
+                var growth = map(x, 0, width, 2.8, 4.);
+                var points = logistic(initVal, growth, 10);
+                curveVertex(x, (1 - points[9]) * height);
+            }
+            endShape();
+            caption(initVal);
+            break;
+        case 1:
+            stroke(0, 20);
+            noFill();
+            var points;
+            var initVal = random(0, 1.);
+            for (var x = 0; x < width; x++) {
+                var growth = map(x, 0, width, 2.8, 4.);
+                points = logistic(initVal, growth, 20);
+                for (var index = 10; index < 20; index += 1.) {
+                    point(x, (1 - points[index]) * height);
+                }
+            }
+            caption(initVal);
+            break;
+        case 2:
+            stroke(0, 20);
+            noFill();
+            var points;
+            var initVal;
+            for (var x = 0; x < width; x += 1) {
+                initVal = Math.random();
+                var growth = map(x, 0, width, 2.8, 4.);
+                points = logistic(initVal, growth, 50);
+                for (var index = 40; index < 50; index += 1) {
+                    point(x, (1 - points[index]) * height);
+                }
+            }
+            break;
+    }
 }
 
 function logistic(init, growth, size) {
-  var response = [];
-  var x = response[0] = init * growth * (1 - init);
-  for (var i = 1; i < size; i++) {
-    x = response[i] = x * growth * (1 - x);
-  }
-  return response;
+    var response = [];
+    var x = response[0] = init * growth * (1 - init);
+    for (var i = 1; i < size; i++) {
+        x = response[i] = x * growth * (1 - x);
+    }
+    return response;
 }
 
 function mapLog(src, smin, smax, dmin, dmax) {
-  var norm = (src - smin) / (smax - smin);
-  return Math.log((norm * Math.LN2) + 1) / Math.log(feigenbaum) * (dmax - dmin) + dmin;
+    var norm = (src - smin) / (smax - smin);
+    return Math.log((norm * Math.LN2) + 1) / Math.log(feigenbaum) * (dmax - dmin) + dmin;
 }
 
 function caption(val) {
-  fill(255, 255);
-  noStroke();
-  rect(0, 0, 200, 50);
-  fill(0);
-  text("init value : " + val.toFixed(5), 20, 20);
-  text("frame rate : " + frameRate().toFixed(2), 20, 40);
+    fill(255, 255);
+    noStroke();
+    rect(0, 0, 200, 50);
+    fill(0);
+    text("init value : " + val.toFixed(5), 20, 20);
+    text("frame rate : " + frameRate().toFixed(2), 20, 40);
 }
 
 function mouseClicked() {
-  mode = (mode + 1) % 2;
-  if (mode === 1) {
-    background(255);
-  }
+    mode = (mode + 1) % 3;
+    if (mode > 0) {
+        background(255);
+    }
 }
